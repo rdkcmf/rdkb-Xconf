937c039c84ed93288722a57af5abda7b00fdfdd7
Sunil S <SunilSivarama_Panicker@cable.comcast.com>
2016-11-29 04:26:59 -0800

    ARRISXB6-2551 : Timezone offset calculation for xconf
    Reason for change: Maintenance window time should be
    based on local time.
    
    Test Procedure: Test by issuing reboot from xconf.
    
    Risks: None
    
    Change-Id: I399db1564484bfcf82a4f5317ceab0c7afa46b8a
    Signed-off-by: Sunil S <SunilSivarama_Panicker@cable.comcast.com>

--- scripts/xb6_firmwareDwnld.sh
+++ scripts/xb6_firmwareDwnld.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -f /etc/device.properties ]
+then
+    source /etc/device.properties
+fi
+
 XCONF_LOG_PATH=/rdklogs/logs
 XCONF_LOG_FILE_NAME=xconf.txt.0
 XCONF_LOG_FILE_PATHNAME=${XCONF_LOG_PATH}/${XCONF_LOG_FILE_NAME}
@@ -770,7 +775,13 @@ while [ $reboot_device_success -eq 0 ]; do
     if [ "$rebootImmediately" == "false" ];then
 
         # Check if still within reboot window
-        reb_hr=`date +"%H"`
+
+	if [ "$UTC_ENABLE" == "true" ]
+	then
+        	reb_hr=`LTime H
+	else
+        	reb_hr=`date +"%H"`
+	fi
 
         if [ $reb_hr -le 4 ] && [ $reb_hr -ge 1 ]; then
             echo "XCONF SCRIPT : Still within current maintenance window for reboot"
@@ -797,8 +808,14 @@ while [ $reboot_device_success -eq 0 ]; do
         while [ $http_reboot_ready_stat -eq 1 ]   
         do     
             sleep 10
-            cur_hr=`date +"%H"`
-            cur_min=`date +"%M"`
+	    if [ "$UTC_ENABLE" == "true" ]
+		then
+			cur_hr=`LTime H`
+			cur_min=`LTime M`
+		else
+            		cur_hr=`date +"%H"`
+            		cur_min=`date +"%M"`
+	   fi
             cur_sec=`date +"%S"`
 
             if [ $cur_hr -le 4 ] && [ $cur_min -le 59 ] && [ $cur_sec -le 59 ];
