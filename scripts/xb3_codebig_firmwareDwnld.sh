e172f63a815ae4adc629e4312d1b709e576aef2f
nhanas001c <Naveenkumar_Hanasi@comcast.com>
2019-08-30 05:27:05 +0000

    RDKB-23505 : Enforce HTTPs download for Downloadable modules (XB3)
    Reason for change: Creating /tmp/.xconfssrdownloadurl if the response from Xconf is 404.
    Test Procedure: Build and Verify.
    Risks: Low
    Signed-off-by: nhanas001c <Naveenkumar_Hanasi@comcast.com>
    
    (cherry picked from commit 69efe50e164cc37854936942f6599c6d80fd7980)
    (cherry picked from commit 3562e224aecbc3679e3ab1116f01f9124fb25c69)
    
    Change-Id: Ie2ed3d70554a8a4cc34b1f53464a67712916e572

--- scripts/xb3_codebig_firmwareDwnld.sh
+++ scripts/xb3_codebig_firmwareDwnld.sh
@@ -655,13 +655,18 @@ getFirmwareUpgDetail()
 
 
         # If a response code of 404 was received, exit
-            elif [ $HTTP_RESPONSE_CODE -eq 404 ]; then
-                retry_flag=0
-                image_upg_avl=0
-                echo_t "XCONF SCRIPT : Response code received is 404" >> $XCONF_LOG_FILE
-                if [ "$isPeriodicFWCheckEnabled" == "true" ]; then
-		   exit
-		fi
+        elif [ $HTTP_RESPONSE_CODE -eq 404 ]; then
+            retry_flag=0
+            image_upg_avl=0
+            echo_t "XCONF SCRIPT : Response code received is 404" >> $XCONF_LOG_FILE
+            if [ "$DEVICE_MODEL" = "TCHXB3" ]; then
+                echo_t "XCONF SCRIPT : Creating /tmp/.xconfssrdownloadurl with $HTTP_RESPONSE_CODE Xconf response"  >> $XCONF_LOG_FILE
+                echo "404" > /tmp/.xconfssrdownloadurl
+            fi
+
+            if [ "$isPeriodicFWCheckEnabled" == "true" ]; then
+                exit
+            fi
         # If a response code of 0 was received, the server is unreachable
         # Try reconnecting
         else
