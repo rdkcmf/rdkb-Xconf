b9b824955009e72c7a8168b4d77eceb40ba3fe52
Harnish Patel <Harnish_Patel@comcast.com>
2019-09-21 00:17:04 +0000

    RDKB-17741 : CPE sends reboot pending notification repeatedly each 5 min
    Reason for change: CPE should send next notification in next maintenance window
    Test Procedure:
    1. Reboot window kicks in, CPE sends pre-reboot notification to XPC (via WebPA)
    2. XPC sends the notification to RMS (after adding XBO ID). Note that no state information is kept in XPC .
    3. User says skip reboot (using XRE)
    4. XPC sends cancel reboot request to CPE
    5. CPE cancels the reboot
    6. CPE should send next notification in next maintenance window
    7. This cycle repeats 5 times.
    8. After 5 skips, CPE goes ahead and reboots.
    Risks: None
    Signed-off-by: Harnish_Patel@comcast.com
    
    (cherry picked from commit 6968c33e630c163ae0a7a788f9c563dfb7d0d30f)
    (cherry picked from commit e228f72fb3a4050e118a6c5b653b4ce3e4cc3c46)
    
    RDKB-17741 : CPE sends reboot pending notification repeatedly each 5 min
    
    Reason for change: CPE should send next notification in next maintenance window
    Test Procedure:
    1. Reboot window kicks in, CPE sends pre-reboot notification to XPC (via WebPA)
    2. XPC sends the notification to RMS (after adding XBO ID). Note that no state information is kept in XPC .
    3. User says skip reboot (using XRE)
    4. XPC sends cancel reboot request to CPE
    5. CPE cancels the reboot
    6. CPE should send next notification in next maintenance window
    7. This cycle repeats 5 times.
    8. After 5 skips, CPE goes ahead and reboots.
    Risks: None
    Signed-off-by: Harnish_Patel@comcast.com
    
    Change-Id: Icc733f236ff715673601a775176c8dd9a5e2e692
    (cherry picked from commit 252784f7a3f6ab3b0031dc2481a507c582cbede9)
    (cherry picked from commit 20a09eab180db5b54f33d43faccea02804e5c082)

--- scripts/xb3_codebig_firmwareDwnld.sh
+++ scripts/xb3_codebig_firmwareDwnld.sh
@@ -1308,9 +1308,20 @@ while [ $reboot_device_success -eq 0 ]; do
                 rm -rf $ABORT_REBOOT
                 rm -rf $deferReboot
                 reboot_device_success=0
-                if [ "$isPeriodicFWCheckEnabled" == "true" ]; then
-                      exit
-                fi
+
+		while [ 1 ]
+		do
+		    checkMaintenanceWindow
+
+		    if [ $reb_window -eq 1 ]
+		    then
+		        #We're still within the maintenance window, sleeping for 2 hr to come out of maintenance window
+		        sleep 7200
+		    else
+		        #If we're out of the maintenance window, exit while loop
+		        break
+		    fi
+		done
        fi
 
      # The reboot ready status didn't change to OK within the maintenance window
